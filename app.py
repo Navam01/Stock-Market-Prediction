1602ea2f219ea8748645c4b3f216238a4e55ba19
Navam01 <navamc1112@gmail.com>
2025-11-02 19:36:04 +0530

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from src.lstm_model import train_lstm_model
 from src.signal_generator import generate_signals
 import numpy as np
 import pandas as pd
-st.title("📈 AI Trading Signal Generator By Kumar Sachin")
+st.title("📈 AI Trading Signal Generator By navam choudhary")
 
 ticker = st.text_input("Enter stock ticker", "AAPL")
 df = get_stock_data(ticker)
